7bc8ad0210ebd5aeaa15cafe1f57002179de3756
gundao <haluantuo@gmail.com>
2023-03-01 22:31:52 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,6 +7,12 @@
 #=================================================
 #1. Modify default IP
 #sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/0.openwrt.pool.ntp.org/ntp.ntsc.ac.cn/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/cn.ntp.org.cn/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time.pool.aliyun.com/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/option leasetime       12h/option leasetime    12h\n    list dhcp_option 6,192.168.1.218/g' /home/gundao/openwrt/package/network/services/dnsmasq/files/dhcp.conf
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
