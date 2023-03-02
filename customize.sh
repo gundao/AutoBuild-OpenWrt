1c56eb1d6072c77b034d060ebe8ceb36fe12669f
gundao <haluantuo@gmail.com>
2023-03-02 11:57:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,7 +12,7 @@ sed -i 's/1.openwrt.pool.ntp.org/cn.ntp.org.cn/g' openwrt/package/base-files/fil
 sed -i 's/2.openwrt.pool.ntp.org/time.pool.aliyun.com/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/3.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/UTC/CST-8/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/option leasetime       12h/option leasetime    12h\n    list dhcp_option 6,192.168.1.218/g' openwrt/package/network/services/dnsmasq/files/dhcp.conf
+#sed -i 's/option leasetime       12h/option leasetime    12h\n    list dhcp_option 6,192.168.1.218/g' openwrt/package/network/services/dnsmasq/files/dhcp.conf
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
